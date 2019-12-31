1993ea2ed64dc7c73bf4de39a503592b9019b3ae
StefanRei <stefan.reimer@tu-dortmund.de>
2019-12-31 17:56:43 +0100

    Added functions to functions.py

--- src/functions/functions.py
+++ src/functions/functions.py
@@ -29,6 +29,92 @@ def replace_hardcoded_columnames(df):
     df.columns = newColumnNames
     return df
 
+def convert_NAs(data, column):
+    """
+    function to convert NAs as np.NaN in a dataFrame    
+    @param df: dataFrame with NAs
+    @return: dataFrame with NAs as np.NaN
+    """
+    df = data.copy()
+    na_amount_before = df[column].isna().sum()
+    df.loc[:, column] = df[column].astype(str)
+    df.loc[:, column] = df[column].str.replace(',', '')
+    df.loc[:, column] = df[column].str.replace('nan', 'NaN')
+    data.loc[:, column] = data[column].str.replace('na', 'NaN')
+    data.loc[:, column] = data[column].str.replace('NaNn', 'NaN')
+    df.loc[:, column] = df[column].replace('NaN', np.NAN)
+    na_amount_after = df[column].isna().sum()
+    nonna_amount_after = df[column].notna().sum()
+    print(f"{na_amount_after - na_amount_before} NAs have been created. "
+          f"{nonna_amount_after} valid values are left. \n")    
+    return df
+
+def get_duplicates(df, column, comment=''):
+    # function to print the sum of duplicates and return the duplicates
+    duplicates = df.loc[:, [column]].duplicated()
+    duplicates_sum = duplicates.sum()
+    print(f'There are {duplicates_sum} duplicates in {column}{comment}.')
+    return duplicates
+
+def find_char_in_colnames(df, char_to_find):
+    """ function to find colnames in a dataFrame with matching subcharacters
+    @param df: dataFrame to search the columns
+    @ param char_to_find: character, which should be searched for as substring in the dataframe
+    @return: array with matching columns
+    """
+    col_names = df.columns.values
+    matching_col_names = col_names[[char_to_find in i for i in col_names]]
+    print(
+        f"columns containing <<{char_to_find}>> are:"
+        "\n"
+        f"{matching_col_names}"
+        "\n")
+    #print(matching_col_names)
+    #print('\t')
+    return matching_col_names
+
+def convert_date(df, column, format='%Y-%m-%d', errors = 'raise'):
+    """
+    function to convert characters to pandas datetime column in a dataFrame and print NA information.
+    
+    @param df: dataFrame with column to convert
+    @param column: column to convert to datetime
+    @param format: look at pd.to_datetime documentation
+    @param errors: look at pd.to_datetime documentation
+    @return: dataFrame with converted column
+    """
+    na_amount_before = df[column].isna().sum()
+    df[column] = pd.to_datetime(df[column], format=format, errors=errors)
+    na_amount_after = df[column].isna().sum()
+    nonna_amount_after = df[column].notna().sum()
+    print(f"{column} has been converted. \n"
+          f"{na_amount_after - na_amount_before} NAs have been created. "
+          f"{nonna_amount_after} valid values are left. \n")    
+    return df
+
+def convert_price(data, column, errors = 'raise'):
+    """
+    function to convert characters to numeric column in a dataFrame and print NA information.
+    
+    @param df: dataFrame with column to convert
+    @return: dataFrame with converted column
+    """
+    df = data.copy()
+    na_amount_before = df[column].isna().sum()
+    df.loc[:, column] = df[column].astype(str)
+    df.loc[:, column] = df[column].str.replace(',', '')
+    df.loc[:, column] = df[column].str.replace('nan', 'NaN')
+    # data.loc[:, column] = data[column].str.replace('na', 'NaN')
+    # data.loc[:, column] = data[column].str.replace('NaNn', 'NaN')
+    df.loc[:, column] = df[column].replace('NaN', np.NAN)
+    df.loc[:, column] = pd.to_numeric(df.loc[:, column], errors=errors)
+    na_amount_after = df[column].isna().sum()
+    nonna_amount_after = df[column].notna().sum()
+    print(f"{column} has been converted. \n"
+          f"{na_amount_after - na_amount_before} NAs have been created. "
+          f"{nonna_amount_after} valid values are left. \n")    
+    return df
+
 # TODO: replace linebreak in array values
 # def replaceLinebreak(string):
 # sdc_data["IPO\nFlag\n(Y/N)"]
